616f96c354b419625134d9aaa7efd2c594cfd391
joshua saubon <joshuasaubon@joshuas-Mac-mini.local>
2021-07-10 20:56:02 +0800

    antmobile

--- src/pages/Application/Application.tsx
+++ src/pages/Application/Application.tsx
@@ -1,28 +1,116 @@
 import {
 	IonContent,
 	IonHeader,
+	IonInput,
+	IonItem,
+	IonItemDivider,
+	IonLabel,
+	IonList,
+	IonListHeader,
 	IonPage,
+	IonSelectOption,
+	IonSelect,
 	IonTitle,
 	IonToolbar,
 } from "@ionic/react";
 import "./Application.css";
+import { InputItem, List } from "antd-mobile";
 
 const Application = () => {
 	return (
 		<IonPage>
 			<IonHeader>
-				<IonToolbar>
-					<IonTitle>Application</IonTitle>
-				</IonToolbar>
+				{/* <IonToolbar> */}
+				{/* <IonTitle>Application Form</IonTitle> */}
+				{/* </IonToolbar> */}
 			</IonHeader>
 			<IonContent fullscreen>
-				<IonHeader collapse="condense">
+				{/* <IonHeader collapse="condense">
 					<IonToolbar>
 						<IonTitle size="large">Application</IonTitle>
 					</IonToolbar>
-				</IonHeader>
+				</IonHeader> */}
 				{/* <ExploreContainer name="Tab 1 page" /> */}
-				<div className="container">test</div>
+				{/* <IonTitle>Application</IonTitle> */}
+				<div className="container">
+					<h1>Application Form</h1>
+					<List>
+						<InputItem type="number" clear >
+							test
+						</InputItem>
+					</List>
+					<IonList>
+						<h4>Basic Info</h4>
+						<IonItem>
+							<IonLabel position="floating">First Name</IonLabel>
+							<IonInput value=""></IonInput>
+						</IonItem>
+						<IonItem>
+							<IonLabel position="floating">Middle Name</IonLabel>
+							<IonInput value=""></IonInput>
+						</IonItem>
+						<IonItem>
+							<IonLabel position="floating">Last Name</IonLabel>
+							<IonInput value=""></IonInput>
+						</IonItem>
+						<IonItem>
+							<IonLabel position="floating">Extension</IonLabel>
+							<IonInput value=""></IonInput>
+						</IonItem>
+						<IonItem>
+							<IonLabel position="floating">Nickname</IonLabel>
+							<IonInput value=""></IonInput>
+						</IonItem>
+						<br />
+						<h4>Other Info</h4>
+						<IonItem>
+							<IonLabel position="floating">Sex</IonLabel>
+							<IonSelect
+								// value={gender}
+								placeholder="Select Sex"
+								interface="popover"
+								// onIonChange={(e) => setGender(e.detail.value)}
+							>
+								<IonSelectOption value="Female">Female</IonSelectOption>
+								<IonSelectOption value="Male">Male</IonSelectOption>
+							</IonSelect>
+						</IonItem>
+						<IonItem>
+							<IonLabel position="floating">Civil Status</IonLabel>
+							<IonSelect
+								// value={gender}
+								placeholder="Select Civil Status"
+								interface="popover"
+								// onIonChange={(e) => setGender(e.detail.value)}
+							>
+								<IonSelectOption value="Single">Single</IonSelectOption>
+								<IonSelectOption value="Married">Married</IonSelectOption>
+								<IonSelectOption value="Widowed">Widowed</IonSelectOption>
+								<IonSelectOption value="Separated">Separated</IonSelectOption>
+							</IonSelect>
+						</IonItem>
+						<IonItem>
+							<IonLabel position="floating">Nationality</IonLabel>
+							<IonInput value=""></IonInput>
+						</IonItem>
+						<IonItem>
+							<IonLabel position="floating">Ethnicity</IonLabel>
+							<IonInput value=""></IonInput>
+						</IonItem>
+						<IonItem>
+							<IonLabel position="floating">Barangay</IonLabel>
+							<IonInput value=""></IonInput>
+						</IonItem>
+						<IonItem>
+							<IonLabel position="floating">Date of Birth</IonLabel>
+							<IonInput value=""></IonInput>
+						</IonItem>
+						<IonItem>
+							<IonLabel position="floating">Place of Birth</IonLabel>
+							<IonInput value=""></IonInput>
+						</IonItem>
+					</IonList>
+				</div>
 			</IonContent>
 		</IonPage>
 	);
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,28 +1,64 @@
 import {
+	IonCard,
+	IonCardContent,
 	IonContent,
 	IonHeader,
+	IonIcon,
 	IonPage,
+	IonSlide,
+	IonSlides,
 	IonTitle,
 	IonToolbar,
 } from "@ionic/react";
+import { newspaperOutline } from "ionicons/icons";
 import "./Home.css";
 
 const Home = () => {
+	const slideOpts = {
+		initialSlide: 1,
+		speed: 400,
+		autoplay: {
+			delay: 10000,
+		},
+		centeredSlides: true,
+		loop: true,
+	};
 	return (
 		<IonPage>
-			<IonHeader>
+			{/* <IonHeader>
 				<IonToolbar>
 					<IonTitle>Home</IonTitle>
 				</IonToolbar>
-			</IonHeader>
+			</IonHeader> */}
 			<IonContent fullscreen>
-				<IonHeader collapse="condense">
+				{/* <IonHeader collapse="condense">
 					<IonToolbar>
 						<IonTitle size="large">Home</IonTitle>
 					</IonToolbar>
-				</IonHeader>
+				</IonHeader> */}
 				{/* <ExploreContainer name="Tab 1 page" /> */}
-				<div className="container">test</div>
+				{/* <div className="container">
+                    
+                </div> */}
+				<IonCard>
+					<IonSlides pager={true} options={slideOpts}>
+						<IonSlide>
+							<h1>Slide 1</h1>
+						</IonSlide>
+						<IonSlide>
+							<h1>Slide 2</h1>
+						</IonSlide>
+						<IonSlide>
+							<h1>Slide 3</h1>
+						</IonSlide>
+					</IonSlides>
+				</IonCard>
+				<div style={{ paddingLeft: 15, paddingRight: 15 }}>
+					<h1 style={{ marginTop: 0 }}>
+						<IonIcon icon={newspaperOutline} className="newsandannouncements_icon"/>
+						News and Announcements
+					</h1>
+				</div>
 			</IonContent>
 		</IonPage>
 	);
