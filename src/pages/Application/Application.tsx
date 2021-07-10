6cee0101dc6858e8caccaa46ab7cd5ad4b230f3c
joshua saubon <joshuasaubon@joshuas-Mac-mini.local>
2021-07-10 22:02:35 +0800

    antdmobile

--- src/pages/Application/Application.tsx
+++ src/pages/Application/Application.tsx
@@ -12,11 +12,34 @@ import {
 	IonSelect,
 	IonTitle,
 	IonToolbar,
+	IonSlides,
+	IonSlide,
 } from "@ionic/react";
 import "./Application.css";
-import { InputItem, List } from "antd-mobile";
+import { WhiteSpace, List, Steps, WingBlank, Card, Button } from "antd-mobile";
+
+const Step = Steps.Step;
 
 const Application = () => {
+	const slideOpts = {
+		initialSlide: 0,
+		speed: 400,
+		allowTouchMove: false,
+		// autoplay: {
+		// 	delay: 10000,
+		// },
+		// centeredSlides: true,
+		// loop: true,
+	};
+	let slider: any | null;
+
+	const handleUpdateSlider = (direction: any) => {
+		if (direction == "next") {
+			slider.slideNext();
+		} else {
+			slider.slidePrev();
+		}
+	};
 	return (
 		<IonPage>
 			<IonHeader>
@@ -33,83 +56,92 @@ const Application = () => {
 				{/* <ExploreContainer name="Tab 1 page" /> */}
 				{/* <IonTitle>Application</IonTitle> */}
 				<div className="container">
-					<h1>Application Form</h1>
-					<List>
-						<InputItem type="number" clear >
-							test
-						</InputItem>
-					</List>
-					<IonList>
-						<h4>Basic Info</h4>
-						<IonItem>
-							<IonLabel position="floating">First Name</IonLabel>
-							<IonInput value=""></IonInput>
-						</IonItem>
-						<IonItem>
-							<IonLabel position="floating">Middle Name</IonLabel>
-							<IonInput value=""></IonInput>
-						</IonItem>
-						<IonItem>
-							<IonLabel position="floating">Last Name</IonLabel>
-							<IonInput value=""></IonInput>
-						</IonItem>
-						<IonItem>
-							<IonLabel position="floating">Extension</IonLabel>
-							<IonInput value=""></IonInput>
-						</IonItem>
-						<IonItem>
-							<IonLabel position="floating">Nickname</IonLabel>
-							<IonInput value=""></IonInput>
-						</IonItem>
-						<br />
-						<h4>Other Info</h4>
-						<IonItem>
-							<IonLabel position="floating">Sex</IonLabel>
-							<IonSelect
-								// value={gender}
-								placeholder="Select Sex"
-								interface="popover"
-								// onIonChange={(e) => setGender(e.detail.value)}
-							>
-								<IonSelectOption value="Female">Female</IonSelectOption>
-								<IonSelectOption value="Male">Male</IonSelectOption>
-							</IonSelect>
-						</IonItem>
-						<IonItem>
-							<IonLabel position="floating">Civil Status</IonLabel>
-							<IonSelect
-								// value={gender}
-								placeholder="Select Civil Status"
-								interface="popover"
-								// onIonChange={(e) => setGender(e.detail.value)}
-							>
-								<IonSelectOption value="Single">Single</IonSelectOption>
-								<IonSelectOption value="Married">Married</IonSelectOption>
-								<IonSelectOption value="Widowed">Widowed</IonSelectOption>
-								<IonSelectOption value="Separated">Separated</IonSelectOption>
-							</IonSelect>
-						</IonItem>
-						<IonItem>
-							<IonLabel position="floating">Nationality</IonLabel>
-							<IonInput value=""></IonInput>
-						</IonItem>
-						<IonItem>
-							<IonLabel position="floating">Ethnicity</IonLabel>
-							<IonInput value=""></IonInput>
-						</IonItem>
-						<IonItem>
-							<IonLabel position="floating">Barangay</IonLabel>
-							<IonInput value=""></IonInput>
-						</IonItem>
-						<IonItem>
-							<IonLabel position="floating">Date of Birth</IonLabel>
-							<IonInput value=""></IonInput>
-						</IonItem>
-						<IonItem>
-							<IonLabel position="floating">Place of Birth</IonLabel>
-							<IonInput value=""></IonInput>
-						</IonItem>
-					</IonList>
+					<WingBlank size="lg">
+						<Steps direction="horizontal" current={0}>
+							<Step title="Waiver"></Step>
+							<Step title="Form"></Step>
+							<Step title="Submit"></Step>
+						</Steps>
+					</WingBlank>
+					<WingBlank size="sm">
+						<IonSlides pager={false} options={slideOpts} ref={(e) => (slider = e)}>
+							<IonSlide>
+								<Card style={{ width: "100%" }}>
+									<Card.Body>
+										<h1>Waiver</h1>
+										Waiver Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+										temporibus possimus illo vero provident perspiciatis laudantium eaque
+										ea natus soluta, impedit at error. Error distinctio optio excepturi
+										impedit? Doloremque, optio? Waiver Lorem ipsum dolor sit amet
+										consectetur adipisicing elit. Eius temporibus possimus illo vero
+										provident perspiciatis laudantium eaque ea natus soluta, impedit at
+										error. Error distinctio optio excepturi impedit? Doloremque, optio?
+										<WhiteSpace />
+										<Button
+											type="primary"
+											size="small"
+											inline
+											icon="right"
+											onClick={(e) => handleUpdateSlider("next")}
+										>
+											Next
+										</Button>
+									</Card.Body>
+								</Card>
+							</IonSlide>
+							<IonSlide>
+								<Card style={{ width: "100%" }}>
+									<Card.Body>
+										<h1>Application Form</h1>
+										<Button
+											type="primary"
+											size="small"
+											inline
+											icon="left"
+											onClick={(e) => handleUpdateSlider("prev")}
+										>
+											Prev
+										</Button>
+										<Button
+											type="primary"
+											size="small"
+											inline
+											icon="right"
+											onClick={(e) => handleUpdateSlider("next")}
+										>
+											Next
+										</Button>
+									</Card.Body>
+								</Card>
+							</IonSlide>
+							<IonSlide>
+								<Card style={{ width: "100%" }}>
+									<Card.Body>
+										<h1>Review and Submit</h1>
+										<Button
+											type="primary"
+											size="small"
+											inline
+											icon="left"
+											onClick={(e) => handleUpdateSlider("prev")}
+										>
+											Prev
+										</Button>
+										<Button
+											type="primary"
+											size="small"
+											inline
+											icon="check"
+											// onClick={(e) => handleUpdateSlider("prev")}
+										>
+											Submit
+										</Button>
+									</Card.Body>
+								</Card>
+							</IonSlide>
+						</IonSlides>
+						{slider && slider.getActiveIndex()}
+					</WingBlank>
 				</div>
 			</IonContent>
 		</IonPage>
